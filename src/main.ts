687cbf7bce81a951671101e98c9f09227843edc8
Lucas Cabral <lucassimple@hotmail.com>
2025-03-13 11:12:01 -0300

    First commit

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { envs } from './config/envs';
+import { RpcCustomExceptionFilter } from './common/exception';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const logger = new Logger('Bootstrap');
+  
+  app.setGlobalPrefix('api/v1');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  
+
+  app.useGlobalFilters( new RpcCustomExceptionFilter )
+
+  await app.listen(envs.port);
+  logger.log('Server running on port', envs.port);
+}
+bootstrap();
---
+++ src/products/products.controller.ts
@@ -0,0 +1,60 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Inject,
+  Query,
+  ParseIntPipe,
+} from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { PRODUCT_SERVICE } from 'src/config';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
+import { PaginationDto } from 'src/common/dto';
+import { catchError, firstValueFrom } from 'rxjs';
+
+@Controller('products')
+export class ProductsController {
+  constructor(
+    @Inject(PRODUCT_SERVICE) private readonly productsClient: ClientProxy,
+  ) {}
+
+  @Post()
+  createProduct(@Body() createProductDto: CreateProductDto) {
+    return this.productsClient.send('create_product', createProductDto);
+  }
+
+  @Get()
+  getProducts(@Query() paginationDto: PaginationDto) {
+    return this.productsClient.send('get_products', paginationDto);
+  }
+
+  @Get(':id')
+  async getProduct( @Param('id', ParseIntPipe ) id: number ) {
+    return this.productsClient.send( 'get_product_by_id', { id } ).pipe(
+      catchError((err) => {
+        throw new RpcException(err);
+      }),
+    );
+  }
+
+  @Patch(':id')
+  updateProduct(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateProductDto: UpdateProductDto,
+  ) {
+    return this.productsClient.send('update_product_by_id', [
+      id,
+      updateProductDto,
+    ]);
+  }
+
+  @Delete(':id')
+  deleteProduct(@Param('id', ParseIntPipe) id: number) {
+    return this.productsClient.send('delete_product_by_id', { id });
+  }
+}
